8fc17bf76d76e15124dd621cb0b0ca5d415eb41c
Dady francisco mendes <dev_dady@outlook.com>
2024-10-30 22:18:31 -0300

    Recurso para criar um novo usuário

--- src/main/java/com/dadyfrancisco/parkapi/park_api/web/controller/UsuarioController.java
+++ src/main/java/com/dadyfrancisco/parkapi/park_api/web/controller/UsuarioController.java
@@ -1,7 +1,12 @@
 package com.dadyfrancisco.parkapi.park_api.web.controller;
 
+import com.dadyfrancisco.parkapi.park_api.entity.Usuario;
 import com.dadyfrancisco.parkapi.park_api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
@@ -11,5 +16,10 @@ public class UsuarioController {
 
     private final UsuarioService service;
 
-    
+    @PostMapping
+    public ResponseEntity<Usuario>create(@RequestBody Usuario usuario){
+        Usuario user = service.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
 }
