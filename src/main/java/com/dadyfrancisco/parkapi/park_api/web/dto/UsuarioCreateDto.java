3544ae5deb2f54cc20826d29e458dc16aa6882dc
Dady francisco mendes <dev_dady@outlook.com>
2024-11-02 19:10:40 -0300

    Validando o objeto UsuarioCreateDto

--- src/main/java/com/dadyfrancisco/parkapi/park_api/web/dto/UsuarioCreateDto.java
+++ src/main/java/com/dadyfrancisco/parkapi/park_api/web/dto/UsuarioCreateDto.java
@@ -1,11 +1,17 @@
 package com.dadyfrancisco.parkapi.park_api.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor @ToString
 public class UsuarioCreateDto {
-
+    @Email(regexp ="Formato do e-mail esta invalido" )
+    @NotBlank
     private  String username;
+    @NotBlank
+    @Size(min = 6,max = 6 )
     private  String password;
 }
