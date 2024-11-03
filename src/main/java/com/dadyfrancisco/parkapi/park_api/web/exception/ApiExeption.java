c6804bcc588fc1a143114d6b769afd4a1e7c7072
Dady francisco mendes <dev_dady@outlook.com>
2024-11-03 01:09:13 -0300

    Tratando exceções lançadas por no processo de validação de campos

---
+++ src/main/java/com/dadyfrancisco/parkapi/park_api/web/exception/ApiExeption.java
@@ -0,0 +1,29 @@
+package com.dadyfrancisco.parkapi.park_api.web.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExeption {
+    private static final Logger log = LoggerFactory.getLogger(ApiExeption.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErroMessage>MethodArgumentNotValidException(MethodArgumentNotValidException ex,
+                                                                      HttpServletRequest request,
+                                                                      BindingResult result){
+        log.error("Api Error -", ex);
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErroMessage(request,HttpStatus.UNPROCESSABLE_ENTITY,"campos invalido(s)",result));
+
+    }
+}
